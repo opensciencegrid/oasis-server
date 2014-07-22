cf850f2cb29782bf1c3c63600c7af6611b7020e5
/DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Jose Caballero 649 </DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Jose Caballero 649@462906bc-84b7-4c8b-9c62-a324b9b949a8>
2014-07-22 14:38:13 +0000

    added creation of /var/run/oasis

--- oasis-server/trunk/misc/rpm-post.sh
+++ oasis-server/trunk/misc/rpm-post.sh
@@ -1,43 +1,82 @@
 #!/bin/bash 
 
-#
-# ensures the condor_oasis_wrapper has execution permissions
-#
-#chmod +x /usr/libexec/condor_oasis_wrapper.sh 
+f_wrapper_permissions(){
+    #
+    # ensures the condor_oasis_wrapper has execution permissions
+    #
 
-#
-# ensures the osg-oasis-update script has execution permissions
-#
-#chmod +x /usr/bin/osg-oasis-update 
+    #chmod +x /usr/libexec/condor_oasis_wrapper.sh 
+}
 
+f_user_client_permissions(){
+    #
+    # ensures the osg-oasis-update script has execution permissions
+    #
 
-# FIXME !! temporary solution
-# creates directory /var/log/oasis/
-if [ ! -d /var/log/oasis ]; then
-    mkdir /var/log/oasis
-    #chmod go+w /var/log/oasis
-    chmod 1777 /var/log/oasis
-fi
-
-### # FIXME !! temporary solution
-### # enforce /var/log/oasis.log is writeable
-### touch /var/log/oasis/oasis.log
-### chmod go+w /var/log/oasis/oasis.log
+    #chmod +x /usr/bin/osg-oasis-update 
+}
     
-
-# FIXME !! temporary solution??
-# creates, if does not exist already, system account "oasis"
-id oasis &> /dev/null
-rc=$?
-if [ $rc -ne 0 ]; then
-    useradd -r -m oasis
-    # adding the sticky bit to /home/oasis
+f_create_log_directory(){
+    #
+    # creates directory /var/log/oasis/
+    # adding the sticky bit 
     # so everyone can write, but only each user
     # can delete her own content
-    chmod 1777 /home/oasis
-fi
+    #
 
+    if [ ! -d /var/log/oasis ]; then
+        mkdir /var/log/oasis
+        chmod 1777 /var/log/oasis
+    fi
+}
 
-# add oasis daemon to checkconfig
-chkconfig --add oasisd
-chkconfig oasisd off
+f_create_oasis_account(){
+    #
+    # creates, if does not exist already, system account "oasis"
+    # adding the sticky bit 
+    # so everyone can write, but only each user
+    # can delete her own content
+    #
+
+    id oasis &> /dev/null
+    rc=$?
+    if [ $rc -ne 0 ]; then
+        useradd -r -m oasis
+        chmod 1777 /home/oasis
+    fi
+}
+
+f_create_run_directory(){
+    #
+    # creates directory /var/run/oasis/
+    # adding the sticky bit 
+    # so everyone can write, but only each user
+    # can delete her own content
+    #
+
+    if [ ! -d /var/run/oasis ]; then
+        mkdir /var/run/oasis
+        chmod 1777 /var/run/oasis
+    fi
+}
+    
+f_chkconfig(){
+    #
+    # add oasis daemon to checkconfig
+    # but set it off. It is up to the sys admin to turn it on.
+    #
+
+    chkconfig --add oasisd
+    chkconfig oasisd off
+}
+
+# ------------------------------------------------------------------------- #  
+#                           M A I N                                         # 
+# ------------------------------------------------------------------------- #  
+
+f_wrapper_permissions
+f_user_client_permissions
+f_create_log_directory
+f_create_oasis_account
+f_create_run_directory
+f_chkconfig
