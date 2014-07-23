11ad59e25ef9c2e57ccddad373e91b4438724a5f
/DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Jose Caballero 649 <jcaballero@bnl.gov>
2014-07-23 15:03:50 +0000

    checking if daemon is running and stop it

--- misc/rpm-pre.sh
+++ misc/rpm-pre.sh
@@ -1,2 +1,24 @@
 #!/bin/bash 
 #
+
+# FIXME: should we check if RC == 1 (dead but PID file exists) or RC == 2 (dead but subsys locked)?
+
+# FIXME: how does this translate to the final spec file?
+
+f_stop_daemon(){
+    #
+    # checks if the daemon is running,
+    # if so, stops it
+    #
+
+    service oasisd status 1>/dev/null
+    rc=$?
+    if [ $rc -ne 0 ]; then
+        # daemon is running...
+        service oasisd stop 1>/dev/null
+    fi
+
+}
+
+
+f_stop_daemon
