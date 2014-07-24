9dd60b64b59687e744a09a0c2d46fc4caf610da9
/DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Jose Caballero 649 <jcaballero@bnl.gov>
2014-07-24 14:16:42 +0000

    cleaning chkconfig on 'uninstall' event

--- misc/rpm-preun.sh
+++ misc/rpm-preun.sh
@@ -1,2 +1,30 @@
 #!/bin/bash 
 #
+
+
+f_stop_daemon(){
+    #
+    # stops the daemon
+    #
+
+    service oasisd stop 1>/dev/null
+}
+
+f_clean_chkconfig(){
+    #
+    # delete oasis daemon to checkconfig
+    # NOTE: only if operation is "uninstall"
+    #
+
+    if [ $1 -eq 0 ]; then
+        chkconfig --del oasisd
+    fi
+}
+
+# ------------------------------------------------------------------------- #  
+#                           M A I N                                         # 
+# ------------------------------------------------------------------------- #  
+
+f_clean_chkconfig $1
+
+
