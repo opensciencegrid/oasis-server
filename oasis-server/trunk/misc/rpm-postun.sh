20c1cc038bb9d4cbd25394a7ee343fd9158ba79e
/DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Jose Caballero 649 </DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Jose Caballero 649@462906bc-84b7-4c8b-9c62-a324b9b949a8>
2014-07-28 15:43:39 +0000

    first code in postun rpm script

--- oasis-server/trunk/misc/rpm-postun.sh
+++ oasis-server/trunk/misc/rpm-postun.sh
@@ -1,2 +1,28 @@
 #!/bin/bash 
 #
+
+f_restart_daemon(){
+    #
+    # checks if the daemon is running,
+    # if so, re-start it
+    #
+
+    # FIXME: should it be done using service oasisd  condrestart ???
+
+    if [ $1 -eq 1 ]; then 
+        #$1 == 1 => upgrade
+        #$1 == 0 => uninstall 
+
+        service oasisd status 1>/dev/null
+        rc=$?
+        if [ $rc -eq 0 ]; then
+            # daemon is running...
+            service oasisd restart 1>/dev/null
+        fi
+}
+
+# ------------------------------------------------------------------------- #  
+#                           M A I N                                         # 
+# ------------------------------------------------------------------------- #  
+
+f_restart_daemon $1
