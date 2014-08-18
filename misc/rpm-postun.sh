93eef96fa6911f4eedfd48239ab1a3327a3d6d81
/DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Jose Caballero 649 <jcaballero@bnl.gov>
2014-08-18 14:28:50 +0000

    using condrestart in postuninstall

--- misc/rpm-postun.sh
+++ misc/rpm-postun.sh
@@ -11,13 +11,7 @@ f_restart_daemon(){
         #$1 == 1 => upgrade
         #$1 == 0 => uninstall 
 
-        service oasisd status 1>/dev/null
-        rc=$?
-        if [ $rc -eq 0 ]; then
-            # daemon is running...
-            service oasisd restart 1>/dev/null
-            # FIXME: should it be done using service oasisd  condrestart ???
-        fi
+        service oasisd condrestart >/dev/null 2>&1
 }
 
 # ------------------------------------------------------------------------- #  
