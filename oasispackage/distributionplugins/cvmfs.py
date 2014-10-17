72c230c8f1b0950cae23cb06993d04a5c651ead3
/DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Jose Caballero 649 <jcaballero@bnl.gov>
2014-10-17 18:52:30 +0000

    code synchronize_back() move to base class cvmfs

--- oasispackage/distributionplugins/cvmfs.py
+++ oasispackage/distributionplugins/cvmfs.py
@@ -6,6 +6,7 @@ import datetime
 import logging
 import os
 import shutil
+import subprocess
 import time
 
 
@@ -34,3 +35,29 @@ class cvmfs(BaseDistribution):
         self.log.info('project /cvmfs/%s exists = %s' %(self.project.projectname, check))
         return check
 
+
+    def synchronize_back(self):
+        """
+        ensure the user scratch area has a perfect copy of what 
+        is currently in the final destination area 
+        """
+
+        self.log.debug('Starting.')
+
+        # FIXME temporary solution ??
+        #       maybe it should be implemented in the distribution plugin?
+        #       for example, to allow easier sync from remote host
+        #
+        destdir = '/cvmfs/%s/%' %(self.project.repository_dest_dir, self.project.project_dest_dir)
+        srcdir = '%s/%' %(self.project.repository_src_dir, self.project.project_src_dir)
+
+        cmd = 'rsync --stats -a -l --delete %s/ %s/' %(destdir, srcdir)
+        self.log.debug('synchronization cmd = %s' %cmd)
+
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        (out, err) = p.communicate()
+        rc = p.returncode
+
+        self.log.debug('Output of synchronization cmd = %s' %out)
+        self.log.debug('Leaving with RC=%s' %rc)
+        return rc
