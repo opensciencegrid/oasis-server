054e01f15dbe9db1606d0ce2dd8a287c2849eadc
/DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Jose Caballero 649 </DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Jose Caballero 649@462906bc-84b7-4c8b-9c62-a324b9b949a8>
2014-10-17 17:56:46 +0000

    adding utils.py

---
+++ oasis-server/trunk/oasispackage/utils.py
@@ -0,0 +1,43 @@
+
+
+def addrepositoryuser(user):
+    '''
+    creates the UNIX ID
+    '''
+
+    uid = self._checkuser(user)
+    if uid:
+        self.log.warning('user %s already exists' %user)
+        return 0
+    else:
+        group = user  # maybe in the future is a generic group name like 'oasis'?
+
+        # FIXME: what about the group?? 
+        # option -g in adduser requires the group to exist !!
+        # or we put all users in the same group... 
+        cmd = '/usr/sbin/adduser -m %s' %user
+        rc, out = commands.getstatusoutput(cmd)
+
+        if rc == 0:
+            self.log.debug('user %s created' %user)
+        else:
+            self.log.critical('user %s creation failed' %user)
+
+        return rc
+
+
+def _checkuser(user):
+    '''
+    checks if that username already exists
+    '''
+    # FIXME is this really needed?
+
+    try:
+        pw = pwd.getpwnam(user)
+        return pw.pw_uid
+    except:
+        return None
+
+
+
+
