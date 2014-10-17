40bea028f8b72adbdd3ebec59790385976609ba3
/DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Jose Caballero 649 </DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Jose Caballero 649@462906bc-84b7-4c8b-9c62-a324b9b949a8>
2014-10-17 18:54:15 +0000

    calling synchronize_back() from cvmfs.py

--- oasis-server/trunk/oasispackage/projects.py
+++ oasis-server/trunk/oasispackage/projects.py
@@ -502,29 +502,33 @@ class Project(ProjectBasicConfig):
 
 
     def _synchronize_back(self):
-        """
-        ensure the user scratch area has a perfect copy of what 
-        is currently in the final destination area 
-        """
-        self.log.debug('Starting.')
+        return self.distributionplugin.synchronize_back()
 
-        # FIXME temporary solution ??
-        #       maybe it should be implemented in the distribution plugin?
-        #       for example, to allow easier sync from remote host
-        #
-        destdir = '/cvmfs/%s/%' %(self.project.repository_dest_dir, self.project.project_dest_dir)
-        srcdir = '%s/%' %(self.project.repository_src_dir, self.project.project_src_dir)
 
-        cmd = 'rsync --stats -a -l --delete %s/ %s/' %(destdir, srcdir)
-        self.log.debug('synchronization cmd = %s' %cmd)
-        
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        (out, err) = p.communicate()
-        rc = p.returncode
+    ###def _synchronize_back(self):
+    ###    """
+    ###    ensure the user scratch area has a perfect copy of what 
+    ###    is currently in the final destination area 
+    ###    """
+    ###    self.log.debug('Starting.')
 
-        self.log.debug('Output of synchronization cmd = %s' %out)
-        self.log.debug('Leaving with RC=%s' %rc)
-        return rc
+    ###    # FIXME temporary solution ??
+    ###    #       maybe it should be implemented in the distribution plugin?
+    ###    #       for example, to allow easier sync from remote host
+    ###    #
+    ###    destdir = '/cvmfs/%s/%' %(self.project.repository_dest_dir, self.project.project_dest_dir)
+    ###    srcdir = '%s/%' %(self.project.repository_src_dir, self.project.project_src_dir)
+
+    ###    cmd = 'rsync --stats -a -l --delete %s/ %s/' %(destdir, srcdir)
+    ###    self.log.debug('synchronization cmd = %s' %cmd)
+    ###    
+    ###    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    ###    (out, err) = p.communicate()
+    ###    rc = p.returncode
+
+    ###    self.log.debug('Output of synchronization cmd = %s' %out)
+    ###    self.log.debug('Leaving with RC=%s' %rc)
+    ###    return rc
 
 
     def runpayload(self, payload):
