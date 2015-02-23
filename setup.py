95570b63c721f14e2c4288fe985c2318f66e52db
/DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Jose Caballero 649 <jcaballero@bnl.gov>
2015-02-23 20:33:45 +0000

    added generate_config_projects to list of util scripts

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ utils_files = ['misc/generate_adduser',
                'misc/generate_gridmap',
                'misc/generate_replicas',
     	       'misc/oasis_replica_status',
+               'misc/generate_config_projects',
               ]
 
 initd_files = ['etc/oasisd']
