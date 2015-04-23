9ed572b2abd7e870d2425f5d740503ba4d6e6ca5
/DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Jose Caballero 649 </DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Jose Caballero 649@462906bc-84b7-4c8b-9c62-a324b9b949a8>
2015-04-23 13:48:06 +0000

    new tag

---
+++ oasis-server/tags/2.0.24/misc/rpm-pre.sh
@@ -0,0 +1,49 @@
+#!/bin/bash 
+#
+
+f_create_oasis_account(){
+    #
+    # creates, if does not exist already, system account "oasis"
+    # adding the sticky bit 
+    # so everyone can write, but only each user
+    # can delete her own content
+    #
+
+    id oasis &> /dev/null
+    rc=$?
+    if [ $rc -ne 0 ]; then
+        useradd -r -m oasis
+        chmod 1777 /home/oasis
+        # FIXME: do we really need the home directory for oasis to have the sticky bit???
+    fi
+}
+
+
+
+f_stop_daemon(){
+    #
+    # checks if the daemon is running,
+    # if so, stops it
+    #
+
+    if [ $1 -eq 2 ]; then 
+        #$1 == 2 => upgrade
+        #$1 == 1 => install 
+        # FIXME: should we check if RC == 1 (dead but PID file exists) or RC == 2 (dead but subsys locked)?
+        # FIXME: how does this translate to the final spec file?
+
+        service oasisd status 1>/dev/null
+        rc=$?
+        if [ $rc -eq 0 ]; then
+            # daemon is running...
+            service oasisd stop 1>/dev/null
+        fi
+    fi
+}
+
+# ------------------------------------------------------------------------- #  
+#                           M A I N                                         # 
+# ------------------------------------------------------------------------- #  
+
+f_create_oasis_account 
+##f_stop_daemon $1
